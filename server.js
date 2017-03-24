c772559433c43bbf2dad5a4018625c37ab3b56e8
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-03-23 22:36:28 -0600

    Extract Foods Controller

--- server.js
+++ server.js
@@ -13,71 +13,19 @@ app.set('port', process.env.PORT || 3000)
 app.get('/', FoodsController.root)
 
 // SHOW (read)
-app.get('/api/foods/:id', (request, response) => {
-  const id = request.params.id
-  Food.find(id).then((data) => {
-    if (!data.rowCount) {
-      return response.sendStatus(404)
-    }
-    response.json(data.rows)
-  });
-}) // end of SHOW
+app.get('/api/foods/:id', FoodsController.show)
 
 // INDEX (read)
-app.get('/api/foods/', (request, response) => {
-  Food.all().then((data) => {
-    if (!data.rowCount) {
-      return response.sendStatus(404)
-    }
-    response.json(data.rows)
-  });
-}) // end of INDEX
+app.get('/api/foods/', FoodsController.index)
 
 // CREATE
-app.post('/api/foods', (request, response) => {
-  const food = request.body
-
-  if (!food) {
-    return response.status(422).send({
-      error: "No food property provided."
-    })
-  } else {
-    Food.createFood(food.name, food.calories, food.visibility).then((data) => {
-      let newFood = data.rows[0]
-      response.status(201).json(newFood)
-    })
-  }
-}) // end CREATE
+app.post('/api/foods', FoodsController.create)
 
 // UPDATE
-app.put('/api/foods/:id', (request, response) => {
-  const id = request.params.id
-  const food = request.body
-
-  Food.update(id, food.name, food.calories, food.visibility)
-  .then((data) => {
-    if (data.rowCount) {
-      Food.find(id).then((data) => {
-        return response.sendStatus(200)
-      })
-    } else {
-      return response.sendStatus(204)
-    }
-  })
-}) // end of UPDATE
+app.put('/api/foods/:id', FoodsController.update)
 
 // DELETE
-app.delete('/api/foods/:id', (request, response) => {
-  const id = request.params.id
-
-  Food.deleteFood(id).then((data) => {
-    if (data.rowCount) {
-      response.sendStatus(200)
-    } else {
-      response.sendStatus(204)
-    }
-  });
-}) // end of DELETE
+app.delete('/api/foods/:id', FoodsController.destroy)
 
 
 if (!module.parent) {
